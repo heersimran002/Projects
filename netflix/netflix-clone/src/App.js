d5cf2b45a7c8d03b4d5bd53fda8c55d9a39e28da
heersimran002 <heersimran002@gmail.com>
2022-10-29 19:36:54 -0500

    banner and rows completed

---
+++ netflix/netflix-clone/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <div className="App">
+      {/* {Nav} */}
+      {/* {Banner} */}
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    </div>
+  );
+}
+
+export default App;
