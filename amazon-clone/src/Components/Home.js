13c55e1d79289016b9979aec68daa65edcaf5310
heersimran002 <heersimran002@gmail.com>
2022-11-09 01:47:27 -0600

    home page background created

---
+++ amazon-clone/src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="">
+        <img
+          className="w-full -z-1 -mb-36 grad home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="bg"
+        />
+        <div>{/* Prodcuts */}</div>
+        <div>{/* 3 products */}</div>
+        <div>{/* 1 Products */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
